87ee20f0f7689e6fef98d035c353071bb8d04b1e
Marc Sallin <marc.sallin@outlook.com>
2015-03-26 21:36:58 +0100

    Bugfix after renaming

--- SQLite.CodeFirst/Properties/AssemblyInfo.cs
+++ SQLite.CodeFirst/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SQLiteEfCodeFirstDbCreator")]
+[assembly: AssemblyTitle("SQLite.CodeFirst")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SQLiteEfCodeFirstDbCreator")]
+[assembly: AssemblyProduct("SQLite.CodeFirst")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
