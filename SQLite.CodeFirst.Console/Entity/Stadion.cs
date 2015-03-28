9aabb8c8d01a79b981ba27b17c671688f4fab951
Marc Sallin <marc.sallin@outlook.com>
2015-03-28 20:36:04 +0100

    Added missing file.

---
+++ SQLite.CodeFirst.Console/Entity/Stadion.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SQLite.CodeFirst.Console.Entity
+{
+    public class Stadion
+    {
+        [Key]
+        [Column(Order = 1)]
+        public string Name { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public string Street { get; set; }
+
+        [Key]
+        [Column(Order = 3)]
+        public string City { get; set; }
+    }
+}
