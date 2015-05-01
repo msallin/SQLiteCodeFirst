aa636adc8da4259b2ea44eb84c7082765b5be7e9
Marc Sallin <marc.sallin@outlook.com>
2015-05-01 15:16:42 +0200

    Issue_21: Added SqliteForeignKeyIndexConvention.

---
+++ SQLite.CodeFirst/Convention/SqliteForeignKeyIndexConvention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SQLite.CodeFirst.Convention
+{
+    public class SqliteForeignKeyIndexConvention : IStoreModelConvention<AssociationType>
+    {
+        public virtual void Apply(AssociationType item, DbModel model)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            if (model == null)
+            {
+                throw new ArgumentNullException("model");
+            }
+            if (item.Constraint == null)
+            {
+                return;
+            }
+        }
+    }
+}
