ecbdc57b9589cb8b278eabe5e8baeb061e144dde
kasajima <kasajima@sunengin.co.jp>
2015-06-19 12:54:22 +0900

    incorrect path returned when dbpath has quotation mark.

--- SQLite.CodeFirst/SqliteConnectionStringParser.cs
+++ SQLite.CodeFirst/SqliteConnectionStringParser.cs
@@ -31,6 +31,8 @@ namespace SQLite.CodeFirst
         public static string GetDataSource(string connectionString)
         {
             var path = ExpandDataDirectory(ParseSqliteConnectionString(connectionString)["data source"]);
+            // remove quotation mark if exists
+            path = path.Trim('"');
             return path;
         }
 
