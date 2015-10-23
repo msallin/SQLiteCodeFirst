a26d7bdffa493586e30c75d983d2d8736dcdcd42
Marc Sallin <marc.sallin@outlook.com>
2015-10-23 20:37:58 +0200

    Added StatementTestBase as a base class for the statement tests.

---
+++ SQLite.CodeFirst.Test/Statement/StatementTestBase.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using SQLite.CodeFirst.Statement;
+
+namespace SQLite.CodeFirst.Test.Statement
+{
+    public abstract class StatementTestBase
+    {
+        protected Mock<IStatement> CreateStatementMock(string createStatementReturnValue)
+        {
+            var statementMock = new Mock<IStatement>();
+            statementMock.Setup(s => s.CreateStatement()).Returns(createStatementReturnValue);
+            return statementMock;
+        }
+    }
+}
