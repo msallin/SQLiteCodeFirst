7e706f6ca11ebedde68fb421860b722b657f224b
Marc Sallin <marc.sallin@outlook.com>
2015-10-23 20:35:14 +0200

    Added InternalsVisibleTo Attribute to make the internals visible to the unit test project.

--- SQLite.CodeFirst/Properties/AssemblyInfo.cs
+++ SQLite.CodeFirst/Properties/AssemblyInfo.cs
@@ -10,6 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Marc Sallin")]
 [assembly: AssemblyCompany("Marc Sallin")]
 
+[assembly: InternalsVisibleTo("SQLite.CodeFirst.Test", AllInternalsVisible = true)]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
