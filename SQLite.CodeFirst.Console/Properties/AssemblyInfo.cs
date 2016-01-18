ded83dcd7e60513848fa06f69f77aceaa41d66a9
Marc Sallin <marc.sallin@garaio.com>
2016-01-18 08:52:22 +0100

    Fix for assembly version patching.

--- SQLite.CodeFirst.Console/Properties/AssemblyInfo.cs
+++ SQLite.CodeFirst.Console/Properties/AssemblyInfo.cs
@@ -2,3 +2,9 @@
 
 [assembly: AssemblyTitle("SQLite.CodeFirst")]
 [assembly: AssemblyDescription("A console application which demonstrates how to use SQLite.CodeFirst.")]
+
+// Will be replaced by the build server
+// But do not put them in a shared file, because this will break the version patching.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
--- SQLite.CodeFirst/Properties/AssemblyInfo.cs
+++ SQLite.CodeFirst/Properties/AssemblyInfo.cs
@@ -5,3 +5,9 @@
     "Creates a SQLite Database from Code, using Entity Framework CodeFirst. " +
     "This Project ships several IDbInitializer which creates " +
     "a new SQLite Database, based on your model/code.")]
+
+// Will be replaced by the build server
+// But do not put them in a shared file, because this will break the version patching.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
