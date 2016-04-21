2bf01214edf5c9a07411e07fdc3d6ddbff19f06e
Marc Sallin <marc.sallin@garaio.com>
2016-04-21 09:34:09 +0200

    #27: Added sample code and updated readme.

--- SQLite.CodeFirst.Console/Entity/Player.cs
+++ SQLite.CodeFirst.Console/Entity/Player.cs
@@ -6,6 +6,7 @@ namespace SQLite.CodeFirst.Console.Entity
     public class Player : Person
     {
         [Index] // Automatically named 'IX_TeamPlayer_Number'
+        [Unique(OnConflictAction.Fail)]
         public int Number { get; set; }
 
         public virtual Team Team { get; set; }
