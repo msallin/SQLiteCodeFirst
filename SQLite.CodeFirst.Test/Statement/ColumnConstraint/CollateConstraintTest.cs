3689a3a3701f5dcd77ebe9fa860128d1fce8be42
Marc Sallin <marc.sallin@garaio.com>
2016-04-21 10:30:57 +0200

    #42: Added CollateConstraint and tests.

---
+++ SQLite.CodeFirst.Test/Statement/ColumnConstraint/CollateConstraintTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLite.CodeFirst.Statement.ColumnConstraint;
+
+namespace SQLite.CodeFirst.Test.Statement.ColumnConstraint
+{
+    [TestClass]
+    public class CollateConstraintTest
+    {
+        [TestMethod]
+        public void CreateStatement_StatementIsCorrect_NoConstraint()
+        {
+            var collationConstraint = new CollateConstraint();
+            collationConstraint.CollationFunction = CollationFunction.None;
+            string output = collationConstraint.CreateStatement();
+            Assert.AreEqual(output, "");
+        }
+
+        [TestMethod]
+        public void CreateStatement_StatementIsCorrect_NoCase()
+        {
+            var collationConstraint = new CollateConstraint();
+            collationConstraint.CollationFunction = CollationFunction.NoCase;
+            string output = collationConstraint.CreateStatement();
+            Assert.AreEqual(output, "COLLATE NOCASE");
+        }
+    }
+}
---
+++ SQLite.CodeFirst/Internal/Statement/ColumnConstraint/CollateConstraint.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace SQLite.CodeFirst.Statement.ColumnConstraint
+{
+    internal class CollateConstraint : IColumnConstraint
+    {
+        private const string Template = "COLLATE {collation-name}";
+
+        public CollationFunction CollationFunction { get; set; }
+
+        public string CreateStatement()
+        {
+            if (CollationFunction == CollationFunction.None)
+            {
+                return string.Empty;
+            }
+
+            var sb = new StringBuilder(Template);
+
+            sb.Replace("{collation-name}", CollationFunction.ToString().ToUpperInvariant());
+
+            return sb.ToString().Trim();
+        }
+    }
+}
