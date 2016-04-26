c3cb75fb0558d8a791804435b3886a8f30f8c22d
Marc Sallin <marc.sallin@garaio.com>
2016-04-26 09:32:36 +0200

    #23: Added test for IndexNameCreator.

---
+++ SQLite.CodeFirst.Test/Builder/NameCreators/IndexNameCreatorTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLite.CodeFirst.Builder.NameCreators;
+
+namespace SQLite.CodeFirst.Test.Builder.NameCreators
+{
+    [TestClass]
+    public class IndexNameCreatorTest
+    {
+        [TestMethod]
+        public void CreateIndexName()
+        {
+            string result = IndexNameCreator.CreateIndexName("MyTable", "MyProperty");
+            Assert.AreEqual("\"IX_MyTable_MyProperty\"", result);
+        }
+
+        [TestMethod]
+        public void CreateIndexNameEscaped()
+        {
+            string result = IndexNameCreator.CreateIndexName("\"base.MyTable\"", "MyProperty");
+            Assert.AreEqual("\"IX_base.MyTable_MyProperty\"", result);
+        }
+    }
+}
