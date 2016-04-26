27ea16ddc9070a0b6415d530bd308e41432531b1
Marc Sallin <marc.sallin@garaio.com>
2016-04-26 09:50:44 +0200

    #23: Added test for HistoryEntityTypeValidator.

---
+++ SQLite.CodeFirst.Test/Utility/HistoryEntityTypeValidator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLite.CodeFirst.Utility;
+
+namespace SQLite.CodeFirst.Test.Utility
+{
+    [TestClass]
+    public class HistoryEntityTypeValidatorTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EnsureValidTypeNotIHistory()
+        {
+            HistoryEntityTypeValidator.EnsureValidType(typeof(InvalidFakeHistoryType1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EnsureValidTypeNoParamLessCtorTest()
+        {
+            HistoryEntityTypeValidator.EnsureValidType(typeof(InvalidFakeHistoryType2));
+        }
+
+        [TestMethod]
+        public void EnsureValidTypeTest()
+        {
+            HistoryEntityTypeValidator.EnsureValidType(typeof(ValidFakeHistoryType));
+        }
+
+        private class ValidFakeHistoryType : IHistory
+        {
+            public int Id { get; set; }
+            public string Hash { get; set; }
+            public string Context { get; set; }
+            public DateTime CreateDate { get; set; }
+        }
+
+        private class InvalidFakeHistoryType1
+        {
+            public int Id { get; set; }
+            public string Hash { get; set; }
+            public string Context { get; set; }
+            public DateTime CreateDate { get; set; }
+        }
+
+        private class InvalidFakeHistoryType2 : IHistory
+        {
+            public InvalidFakeHistoryType2(string test)
+            { }
+
+            public int Id { get; set; }
+            public string Hash { get; set; }
+            public string Context { get; set; }
+            public DateTime CreateDate { get; set; }
+        }
+    }
+}
