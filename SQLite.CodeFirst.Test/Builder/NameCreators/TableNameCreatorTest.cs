3bda882f0eb92554371e232d1bf40703ad3d6a06
Marc Sallin <marc.sallin@garaio.com>
2016-04-26 09:32:10 +0200

    #23: Added test for TableNameCreator.

---
+++ SQLite.CodeFirst.Test/Builder/NameCreators/TableNameCreatorTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLite.CodeFirst.Builder.NameCreators;
+
+namespace SQLite.CodeFirst.Test.Builder.NameCreators
+{
+    [TestClass]
+    public class TableNameCreatorTest
+    {
+        [TestMethod]
+        public void CreateTableNameTest()
+        {
+            string result = TableNameCreator.CreateTableName("Test");
+            Assert.AreEqual("\"Test\"", result);
+        }
+    }
+}
