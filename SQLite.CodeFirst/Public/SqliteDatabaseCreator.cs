dea7e35d69880ac0bf498bf3a6ec5eefa919b1bd
Marc Sallin <marc.sallin@outlook.com>
2019-01-26 15:36:00 +0100

    #139: Deleted unnecessary transaction creation.

--- SQLite.CodeFirst/Public/SqliteDatabaseCreator.cs
+++ SQLite.CodeFirst/Public/SqliteDatabaseCreator.cs
@@ -8,6 +8,11 @@ namespace SQLite.CodeFirst
     /// <summary>
     /// Creates a SQLite-Database based on a Entity Framework <see cref="Database"/> and <see cref="DbModel"/>.
     /// This creator can be used standalone or within an initializer.
+    /// <remark>
+    /// The generated DDL-SQL will be executed together as one statement.
+    /// If there is a open transaction on the Database, the statement will be executed within this transaction.
+    /// Otherwise it will be executed within a own transaction. In anyway the atomicity is guaranteed.
+    /// </remark>
     /// </summary>
     public class SqliteDatabaseCreator : IDatabaseCreator
     {
