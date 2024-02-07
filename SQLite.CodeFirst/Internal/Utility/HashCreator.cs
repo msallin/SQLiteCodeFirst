a79bda11ca83e37c19788b3f2906d1093de92c19
Sergio Martín Fernández <sf101920@harriscomputer.com>
2024-02-07 16:40:11 +0100

    Make SQLLiteCodeFirst FIPS compliant
    This PR makes SQLiteCodeFirst FIPS compliant, needed to operate on US federal or many state government systems as it is a requirement for software vendors, replacing the SHA512Managed class with the SHA512CryptoServiceProvider.
    
    What is FIPS compliance? FIPS stands for Federal Information Processing Standards and are US Government standards that provide a benchmark for implementing cryptographic software
    
    SHA512CryptoServiceProvider uses the FIPS 140-2 validated (FIPS = Federal Information Processing Standards) Crypto Service Provider (CSP) while SHA256Managed does not.

--- SQLite.CodeFirst/Internal/Utility/HashCreator.cs
+++ SQLite.CodeFirst/Internal/Utility/HashCreator.cs
@@ -9,7 +9,7 @@ namespace SQLite.CodeFirst.Utility
         public static string CreateHash(string data)
         {
             byte[] dataBytes = Encoding.ASCII.GetBytes(data);
-            using (SHA512 sha512 = new SHA512Managed())
+            using (SHA512 sha512 = SHA512.Create())
             {
                 byte[] hashBytes = sha512.ComputeHash(dataBytes);
                 string hash = Convert.ToBase64String(hashBytes);
